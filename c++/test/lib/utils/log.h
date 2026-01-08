ef06bc1fb24e877cb9166ca2c0b9ee6d712a7452
Ronan L <aeltorio@universidadatlantico.org>
2026-01-08 13:03:16 +0100

    logs depends on RUST_LOG

--- c++/test/lib/utils/log.h
+++ c++/test/lib/utils/log.h
@@ -4,6 +4,8 @@
 #ifdef HAVE_LOG
 
 #include <cstdio>
+#include <cstdlib>
+#include <string>
 
 // clang-format off
 #define ANSI_BOLD        "\x1b[1m"
@@ -14,20 +16,63 @@
 #define ANSI_RESET       "\x1b[0m"
 // clang-format on
 
+// Log levels: 0=off, 1=error, 2=warn, 3=info, 4=debug
+static inline int _get_log_level() {
+  const char* level_env = std::getenv("RUST_LOG");
+  if (!level_env) {
+    return 2;  // Default: warn level
+  }
+  
+  std::string level_str(level_env);
+  
+  // Check if "debug" is in the string
+  if (level_str.find("debug") != std::string::npos || level_str.find("microtex=debug") != std::string::npos) {
+    return 4;  // Debug level
+  }
+  if (level_str.find("info") != std::string::npos) {
+    return 3;  // Info level
+  }
+  if (level_str.find("warn") != std::string::npos) {
+    return 2;  // Warn level
+  }
+  if (level_str.find("error") != std::string::npos) {
+    return 1;  // Error level
+  }
+  if (level_str.find("off") != std::string::npos) {
+    return 0;  // Off level
+  }
+  
+  return 2;  // Default: warn level
+}
+
 #define dbg(format, ...)                                                              \
-  fprintf(                                                                            \
-    stdout,                                                                           \
-    "FILE: " ANSI_UNDERLINE "%s" ANSI_RESET ", LINE: " ANSI_COLOR_RED "%d" ANSI_RESET \
-    ", FUNCTION: " ANSI_COLOR_CYAN "%s" ANSI_RESET ", MSG: " format,                  \
-    __FILE__,                                                                         \
-    __LINE__,                                                                         \
-    __FUNCTION__,                                                                     \
-    ##__VA_ARGS__                                                                     \
-  );
+  do {                                                                               \
+    if (_get_log_level() >= 1) {                                                    \
+      fprintf(                                                                       \
+        stdout,                                                                      \
+        "FILE: " ANSI_UNDERLINE "%s" ANSI_RESET ", LINE: " ANSI_COLOR_RED "%d" ANSI_RESET \
+        ", FUNCTION: " ANSI_COLOR_CYAN "%s" ANSI_RESET ", MSG: " format,            \
+        __FILE__,                                                                    \
+        __LINE__,                                                                    \
+        __FUNCTION__,                                                                \
+        ##__VA_ARGS__                                                                \
+      );                                                                             \
+    }                                                                                \
+  } while (0)
 
-#define logv(format, ...) fprintf(stdout, format, ##__VA_ARGS__);
+#define logv(format, ...) \
+  do { \
+    if (_get_log_level() >= 4) { \
+      fprintf(stdout, format, ##__VA_ARGS__); \
+    } \
+  } while (0)
 
-#define loge(format, ...) fprintf(stderr, format, ##__VA_ARGS__);
+#define loge(format, ...) \
+  do { \
+    if (_get_log_level() >= 1) { \
+      fprintf(stderr, format, ##__VA_ARGS__); \
+    } \
+  } while (0)
 
 #endif  // HAVE_LOG
 
